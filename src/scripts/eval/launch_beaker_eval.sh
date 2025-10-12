0648fc289c3cc8424b08280d3f3671637c4cce9c
ryanyxw <ryanywan@usc.edu>
2025-10-11 17:21:34 -0700

    updated FlexOlmo fixes for piqa, socialiqa, and mmlu_pro

--- src/scripts/eval/launch_beaker_eval.sh
+++ src/scripts/eval/launch_beaker_eval.sh
@@ -20,8 +20,8 @@ TASKS=(
 #    csqa:mc::olmes
 #    hellaswag:mc::olmes
 #    openbookqa:mc::olmes
-#    piqa:mc::olmes
-#    socialiqa:mc::olmes
+    piqa:mc::olmes
+    socialiqa:mc::olmes
 #    winogrande:mc::olmes
 #
 #    arc_easy:rc::olmes
@@ -30,8 +30,8 @@ TASKS=(
 #    csqa:rc::olmes
 #    hellaswag:rc::olmes
 #    openbookqa:rc::olmes
-#    piqa:rc::olmes
-#    socialiqa:rc::olmes
+    piqa:rc::olmes
+    socialiqa:rc::olmes
 #    winogrande:rc::olmes
 #
 #    # Gen5 tasks
@@ -42,16 +42,17 @@ TASKS=(
 #    drop::olmes
 #
 #    # MMLU tasks
-    mmlu:mc::olmes
-    mmlu_pro_mc::none
+#    mmlu:mc::olmes
+    mmlu_pro:mc::none
 
-    mmlu:rc::olmes
+#    mmlu:rc::olmes
+    mmlu_pro:rc::none
 
     # AGI eval
-    agi_eval_english:1shot::olmes
+#    agi_eval_english:1shot::olmes
 
     # BBH
-    bbh:cot-v1::olmes
+#    bbh:cot-v1::olmes
 
     # Math2 tasks
 #    gsm8k::olmes
@@ -64,10 +65,10 @@ TASKS=(
 #    minerva_math_precalculus::olmes
 #
 #    # Code4 tasks
-    codex_humaneval:temp0.8
-    codex_humanevalplus:temp0.8
-    mbpp::none
-    mbppplus::none
+#    codex_humaneval:temp0.8
+#    codex_humanevalplus:temp0.8
+#    mbpp::none
+#    mbppplus::none
 
 )
 
