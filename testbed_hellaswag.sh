9419c9961278f5f50a939ae0a58087e71f4bbd64
ryanyxw <ryanywan@usc.edu>
2025-10-13 07:56:27 -0700

    added testbed on hellaswag for expert specialization testing

---
+++ testbed_hellaswag.sh
@@ -0,0 +1,9 @@
+python -m offline_evals.run_eval\
+    --task '{"task_name": "hellaswag", "split": "validation", "primary_metric": "acc_per_char", "num_shots": 5, "limit": 1000, "fewshot_source": "OLMES:hellaswag", "metadata": {"regimes": ["OLMES-v0.1"], "alias": "hellaswag:rc::olmes"}}'\
+    --batch-size 4 \
+    --output-dir /weka/oe-training-default/ryanwang/phdbrainstorm/evals/testbed_hellaswag \
+    --save-raw-requests true \
+    --num-workers 1 \
+    --gpus 1 \
+    --model /weka/oe-training-default/ryanwang/phdbrainstorm/models/olmoe-pretrain-replicate/step30995-hf \
+    --model-args '{"model_type": "hf"}'
